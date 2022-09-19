3a89b67906b508cae7f1f66d7f74faefbf3accc2
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-09-18 20:07:36 -0500

    add infra

--- aws/app.py
+++ aws/app.py
@@ -4,7 +4,6 @@ import aws_cdk as cdk
 
 from aws.aws_stack import PipelineStack
 
-
 app = cdk.App()
 PipelineStack(app, "pipeline",
   connection_arn="arn:aws:codestar-connections:us-east-1:180847865862:connection/3f9f0372-2d5e-4e81-a4c5-c3c0446d401a",
--- aws/aws/aws_stack.py
+++ aws/aws/aws_stack.py
@@ -4,6 +4,8 @@ from aws_cdk import (
     pipelines
 )
 
+from aws.infrastructure import InfrastructureStage
+
 
 class PipelineStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, connection_arn: str, **kwargs):
@@ -16,19 +18,30 @@ class PipelineStack(Stack):
       connection_arn=connection_arn
     )
     
+    base_commands = [
+        "cd aws",
+        "npm install -g aws-cdk",
+        "pip install -r requirements.txt",
+    ]
+
     # pipeline created to source from GitHub repo and synthesize a Cloud Assembly
     # from within the aws directory of the project
     pipeline = pipelines.CodePipeline(self, "Pipeline",
       synth=pipelines.ShellStep("Synth",
         input=source,
         primary_output_directory="aws/cdk.out",
-        commands=[
-          "cd aws",
-          "npm install -g aws-cdk",
-          "pip install -r requirements.txt",
+        commands=base_commands + [
           "npx cdk synth"
       ])
     )
-
-
+    
+    infra_stage = InfrastructureStage(self, "Infra")
+    infra_deploy = pipeline.add_stage(infra_stage)
+    infra_deploy.add_pre(pipelines.ShellStep(
+        "Test Infra",
+        commands=base_commands + [
+            "pip install -r requirements-dev.txt",
+            "pytest"
+        ]
+    ))
 
